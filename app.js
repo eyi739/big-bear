69c0053aafb468c814900646db8d54418d036e7b
eyi739 <168599229+eyi739@users.noreply.github.com>
2024-06-05 21:29:58 -0700

    created show page.

--- app.js
+++ app.js
@@ -33,8 +33,7 @@ app.get('/products', async(req, res) => {
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
-    console.log(product);
-    res.send('details page!');
+    res.render('products/show', { product });
 })
 
 app.get('/makeproduct', async (req, res) => {
