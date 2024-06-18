9a8b877af7e6573923784f2fa6346c3af04174a2
eyi739 <168599229+eyi739@users.noreply.github.com>
2024-06-17 23:41:34 -0700

    route functions to properly display links

--- app.js
+++ app.js
@@ -33,8 +33,16 @@ app.get('/', (req, res) => {
 })
 
 app.get('/products', async(req, res) => {
+    const { category } = req.query;
+    if(category) {
+        const products = await Product.find({category})
+        res.render ('products/index', { products, category})
+    } else {
+        const products = await Product.find({})
+        res.render ('products/index', { products, category: 'All' })
+    }
     const products = await Product.find({});
-    res.render ('products/index', { products });
+    
 })
 
 app.get('/products/new', (req, res) => {
