a2eeb2ca57198da172bd9c10ab989470101e0f69
eyi739 <168599229+eyi739@users.noreply.github.com>
2024-06-18 23:10:48 -0700

    seeding.

---
+++ seeds/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const Product = require('../models/product');
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://127.0.0.1:27017/bigBear');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const seedDB = async () => {
+    await Product.deleteMany({})
+    const p = new Product({name: 'Celery', price: 30.00});
+    await p.save();
+}
+
+seedDB();
